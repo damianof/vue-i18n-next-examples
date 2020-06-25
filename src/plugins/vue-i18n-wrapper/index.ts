75337d4d7251d84d9b5d55924bec2ba6d1480c8c
Damiano <damiano@iMacRetina.local>
2020-06-25 12:54:28 -0400

    initial commit

---
+++ src/plugins/vue-i18n-wrapper/index.ts
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueI18n, { LocaleMessages } from 'file:../vue-i18n'
+
+Vue.use(VueI18n)
+
+// export const i18n = new VueI18n({
+// 	locale: 'fr-FR',
+// 	fallbackLocale: 'en-US',
+// 	messages: {
+// 		'en-US': {
+// 			welcome: 'Welcome: this message is localized for English'
+// 		},
+// 		'it-IT': {
+// 			welcome: 'Benvenuti: this message is localized for Italian'
+// 		}
+// 	}
+// })
+
+export const i18n = {}
