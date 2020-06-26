5fa0638fa45fc0e6ef462ec4ec18fea5117f59d0
Damiano <damiano@iMacRetina.local>
2020-06-26 11:44:18 -0400

    configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,25 +3,29 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+  "extends": [
+    "plugin:vue/vue3-essential",
+	"@vue/typescript"
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    //ecmaVersion: 2020
+	parser: "@typescript-eslint/parser"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-	'no-mixed-spaces-and-tabs': 0,
-	'@typescript-eslint/no-explicit-any': 0
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+	"one-var": 0,
+	"no-tabs": 0,
+	"indent": 0,
+	"no-mixed-spaces-and-tabs": 0,
+	"no-multiple-empty-lines": 0,
+	"@typescript-eslint/no-explicit-any": 0
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
         mocha: true
---
+++ vue.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+module.exports = {
+	devServer: {
+		port: 8088
+	},
+	/*pluginOptions: {
+		i18n: {
+			locale: 'en',
+			fallbackLocale: 'en',
+			localeDir: 'locales',
+			enableInSFC: true
+		}
+	},*/
+	productionSourceMap: true,
+	outputDir: 'dist',
+	/* publicPath is used by npm run build to prefix references for script/css files */
+    /*publicPath: process.env.NODE_ENV == 'production' ? './some-prefix/dist/' : './',*/
+	publicPath: './',
+	/* chainWebpack/configureWebpack so that npm run build does not produce hashed file names under dist/ folder */
+    chainWebpack: (config) => {
+
+		config.resolve.alias.set('@', path.resolve(__dirname, 'src'))
+
+        if (config.plugins.has('extract-css')) {
+            const extractCSSPlugin = config.plugin('extract-css');
+            extractCSSPlugin && extractCSSPlugin.tap(() => [{
+                filename: '[name].css',
+                chunkFilename: '[name].css'
+            }])
+        }
+
+		if (process.env.NODE_ENV === 'production') {
+			// if we need to make exception for some things that should not go into the dist folder
+		}
+
+		if (process.env.NODE_ENV === 'coverage') {
+			// // config.devtool('cheap-module-eval-source-map');
+			config.module.rule('ts')
+				.test(/\.ts$/)
+				.use('istanbul-instrumenter-loader')
+				.loader('istanbul-instrumenter-loader')
+				.before('babel-loader')
+				.options({
+					esModules: true
+				})
+
+			config.module.rule('vue')
+				.test(/\.vue$/)
+				.use('vue-loader')
+				.loader('vue-loader')
+				.before('vue-loader')
+				.options({
+					esModules: true
+				})
+		}
+    },
+    configureWebpack: {
+        output: {
+            filename: '[name].js',
+            chunkFilename: '[name].js'
+        },
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, 'src')
+			}
+		}
+    }
+
+}
