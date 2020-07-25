6250b6fa9c5b1c542864cbef31541474167534e6
Damiano Fusco <damianofusco@yahoo.com>
2020-07-25 15:58:44 -0400

    more unit tests

--- tests/unit/models/i18n.spec.ts
+++ tests/unit/models/i18n.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai'
+import _ from 'underscore'
 import { i18n } from '@/plugins/vue-i18n-next-plugin'
 
 describe('i18n', () => {
@@ -23,6 +24,59 @@ describe('i18n', () => {
     describe('fallbackLocale', () => {
       it('should equal ja-JP', () => {
         expect(i18n.global.fallbackLocale.value).to.equal('ja-JP')
+      })
+    })
+
+    describe('datetimeFormats', () => {
+      it('ja-JP should be as expected', () => {
+        const formats = i18n.global.datetimeFormats.value
+
+        const expected: any = {
+          long: {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+          }
+        }
+        expect(_.isEqual(formats['ja-JP'], expected)).to.be.true
+      })
+
+      it('en-US should be as expected', () => {
+        const formats = i18n.global.datetimeFormats.value
+
+        const expected: any = {
+          long: {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+          }
+        }
+        expect(_.isEqual(formats['en-US'], expected)).to.be.true
+      })
+
+      it('it-IT should be as expected', () => {
+        const formats = i18n.global.datetimeFormats.value
+
+        const expected: any = {
+          long: {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+          }
+        }
+        expect(_.isEqual(formats['it-IT'], expected)).to.be.true
       })
     })
   })
